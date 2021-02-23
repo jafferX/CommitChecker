18cf71485e54badf488ffba40e325f3493d3ddb1
jafferX <eriklab1222@gmail.com>
2021-02-23 13:07:02 -0800

    wrote one unit test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -12,3 +12,8 @@ import renderer from 'react-test-renderer';
 it('renders correctly', () => {
   renderer.create(<App />);
 });
+
+it('has child a element', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree.children.length).toBe(1);
+})
